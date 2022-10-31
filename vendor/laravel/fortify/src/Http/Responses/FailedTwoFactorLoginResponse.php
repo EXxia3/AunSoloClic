754cf5e65f166c15488090c1cb351cae5948b9fd
EXxia3 <declaradosinocentes@hotmail.com>
2022-10-31 11:45:25 -0300

    All

---
+++ vendor/laravel/fortify/src/Http/Responses/FailedTwoFactorLoginResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laravel\Fortify\Http\Responses;
+
+use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Contracts\FailedTwoFactorLoginResponse as FailedTwoFactorLoginResponseContract;
+
+class FailedTwoFactorLoginResponse implements FailedTwoFactorLoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        [$key, $message] = $request->has('recovery_code')
+            ? ['recovery_code', __('El código de recuperación de dos factores proporcionado no es válido.')]
+            : ['code', __('El código de autenticación de dos factores proporcionado no es válido.')];
+
+        if ($request->wantsJson()) {
+            throw ValidationException::withMessages([
+                $key => [$message],
+            ]);
+        }
+
+        return redirect()->route('two-factor.login')->withErrors([$key => $message]);
+    }
+}
